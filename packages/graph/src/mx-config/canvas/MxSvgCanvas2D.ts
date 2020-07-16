86ae3d776bb3d709aff8fefb241e4713d8a18c64
Kristian Mandrup <kmandrup@gmail.com>
2020-07-16 15:36:56 +0100

    extract mx-config with mxgraph core class extensions

--- packages/graph/src/mx-config/canvas/MxSvgCanvas2D.ts
+++
@@ -1,8 +0,0 @@
-import mx from "@mxgraph-app/mx";
-const { mxSvgCanvas2D } = mx;
-import { Class } from "../Class";
-
-export const MxSvgCanvas2D = Class.extend([mxSvgCanvas2D], {
-  // Alternative text for unsupported foreignObjects
-  foAltText: "[Not supported by viewer]",
-});
--- packages/graph/src/mx-config/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./stencil-registry";
-export * from "./helpers";
-export * from "./stencil";
-export * from "./sidebar";
-export * from "./touch";
-export * from "./vertex-handler";
